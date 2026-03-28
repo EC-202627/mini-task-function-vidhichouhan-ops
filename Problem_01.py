cc5942d1fe88467cfac5eb894a35efd433fa0ae3
vidhichouhan-ops <vidhi.chouhan@avantika.edu.in>
2026-03-28 17:46:40 +0000

    executed

---
+++ Problem_01.py
@@ -0,0 +1,14 @@
+def calculate_fine(book_title, days_overdue, daily_rate=5.0, max_fine=150.0):
+    fine = days_overdue * daily_rate
+    if fine > max_fine:
+        fine = max_fine
+    return fine
+
+book_title = input()
+days_overdue = int(input())
+
+fine = calculate_fine(book_title, days_overdue)
+
+print(f"Book: {book_title}")
+print(f"Days overdue: {days_overdue}")
+print(f"Fine: Rs. {fine}")
